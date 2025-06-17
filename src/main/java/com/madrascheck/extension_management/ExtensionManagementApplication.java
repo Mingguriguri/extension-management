732f81fa1a85796dcd8989ccb5ca2af849d44724
Minjeong Kim <rlaalswjd-@naver.com>
2025-06-17 16:20:51 +0900

    feat: Spring Boot 프로젝트 초기화

---
+++ src/main/java/com/madrascheck/extension_management/ExtensionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.madrascheck.extension_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionManagementApplication.class, args);
+	}
+
+}
