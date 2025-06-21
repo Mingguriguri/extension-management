2707afc8fe50a2a455a73716716795d8e619b888
Minjeong Kim <rlaalswjd-@naver.com>
2025-06-21 19:23:38 +0900

    chore(extension): 초기 extension 테이블 스키마 및 FIXED 확장자 데이터 추가 - schema.sql: extension 테이블 생성 DDL 추가 - data.sql: bat, cmd, com  FIXED 확장자 초기 데이터 삽입 등

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,9 @@
+-- src/main/resources/data.sql
+INSERT INTO extension (name, type, enabled) VALUES
+            ('bat',  'FIXED', false),
+            ('cmd',  'FIXED', false),
+            ('com',   'FIXED', false),
+            ('cpl',   'FIXED', false),
+            ('exe',  'FIXED', false),
+            ('scr',  'FIXED', false),
+            ('js',  'FIXED', false);
