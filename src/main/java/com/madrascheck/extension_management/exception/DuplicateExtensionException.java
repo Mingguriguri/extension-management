0c6f7060f3e78c7b720f0092840906c38a283e9a
Minjeong Kim <rlaalswjd-@naver.com>
2025-06-21 21:11:36 +0900

    feat(exception): 예외 상태 코드 분리 - DuplicateExtensionException 은 Conflict(409)로,   ExtensionLimitExceededException 은 Bad Request(400)로 상태 코드를 다르게 설정 - DuplicateExtensionException의 경우, 중복된 값은 리소스의 충돌을 의미하기에 상태코드 409로 변경

--- src/main/java/com/madrascheck/extension_management/exception/DuplicateExtensionException.java
+++ src/main/java/com/madrascheck/extension_management/exception/DuplicateExtensionException.java
@@ -3,7 +3,7 @@ package com.madrascheck.extension_management.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateExtensionException extends RuntimeException {
     public DuplicateExtensionException(String message) {
         super(message);
